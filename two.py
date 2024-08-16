791b8123087984ff3fe5c4c5219af2513a35c025
Srikanth-Neelakandan <167616562+Srikanth-Neelakandan@users.noreply.github.com>
2024-08-16 14:48:07 +0530

    Add files via upload

---
+++ two.py
@@ -0,0 +1,75 @@
+import cv2
+import time
+import numpy as np
+import mediapipe as mp
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+#################################
+wCam, hCam = 640, 480
+#################################
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+# Initialize MediaPipe Hands
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.5)
+
+# Audio utilities setup
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
+
+vol = 0
+volBar = 400
+volPer = 0
+
+while True:
+    success, img = cap.read()
+    if not success:
+        continue
+    
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_rgb)
+    
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            x1, y1 = int(hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x * wCam), int(hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y * hCam)
+            x2, y2 = int(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * wCam), int(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * hCam)
+            cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+            
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            
+            length = math.hypot(x2 - x1, y2 - y1)
+            vol = np.interp(length, [50, 300], [minVol, maxVol])
+            volBar = np.interp(length, [50, 300], [400, 150])
+            volPer = np.interp(length, [50, 300], [0, 100])
+            
+            volume.SetMasterVolumeLevel(vol, None)
+            print(int(length), vol)
+            
+            if length < 50:
+                cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+                cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+                cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+    
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+    cv2.imshow("Img", img)
+    cv2.waitKey(1)
